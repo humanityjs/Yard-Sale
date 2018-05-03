28fce08a4ede2f9a1272d80ddc7f6d3e1e65caf8
Mayowa Makinde <mayowa.makinde@andela.com>
2018-05-03 17:59:01 +0100

    setup sequelize and socket.io

--- server/server.js
+++ server/server.js
@@ -2,7 +2,13 @@
 const express = require('express');
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.listen(PORT, () => {
   console.log(`BuyIt is running on port:${PORT}`);
